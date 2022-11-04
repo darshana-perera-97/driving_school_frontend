e23393f70228ea460084a90e649e69ebf9eb0546
Darshana Perera <darshana.saluka.10@gmail.com>
2022-11-04 09:38:12 +0530

    basic structure created

---
+++ src/Design/Design.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Common/Footer";
+import TopBar from "./Common/TopBar";
+import Pages from "./Pages/Pages";
+
+export default function Design() {
+  return (
+    <div>
+      <TopBar />
+      <Pages />
+      <Footer />
+    </div>
+  );
+}
